50075377e5cbc856ec40270032468604d8745dd0
Joshua <62268199+minimalsm@users.noreply.github.com>
2024-03-24 11:11:31 +0000

    Add non-failing error

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const app = express()
 const PORT = process.env.PORT || 8080
 
 app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+  res.send('4') // change this string to ensure a new version deployed
 })
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
-  this_causes_error
+app.listen(PORT + 1, () => {
   console.log(`server started on port ${PORT}`)
 })
